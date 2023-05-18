1c1a153ad213cbb8b34ebb5cc38b2e37a58f8fb2
rpanja2007 <101450382+rpanja2007@users.noreply.github.com>
2023-05-18 04:43:20 +0000

    all in env

--- util/config.js
+++ util/config.js
@@ -2,14 +2,14 @@
 module.exports = {
   fs: require("fs"),
   onboot: require('./onboot'),
-  prefix: process.env.prefix,
+  prefix: process.env.prefix, //put your prefix on secrets
   token: process.env.token, //put token on Secrets
   b_id: process.env.b_id, //put b_id on Secrets 
   b_key: process.env.b_key, //put b_key on Secrets 
-  help_image: process.env.help_image, //put The Link Of image for help image view like A Intro.
+  help_image: process.env.help_image, //put The Link Of image for help image view like A Intro on Secrets.
   color: "#cba6ff",
   wrongcolor: "#dba96b",
-  dev: process.env.dev,
-  footertext: process.env.footertext,
-  footericon: process.env.footericon
+  dev: process.env.dev, //put your discord ID on Secrets
+  footertext: process.env.footertext, //put your footertext on secrets
+  footericon: process.env.footericon //put your footericon link on secrets
 }
